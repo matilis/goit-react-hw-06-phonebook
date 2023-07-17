6f1f199a0b49733680c4a819e4f1efff16fc8eb9
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-07-17 21:05:35 +0200

    HW 6 ver 1.2

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -25,7 +25,7 @@ export const ContactForm = () => {
           <input
             type="text"
             name="name"
-            pattern="^[a-zA-Z]+(([' \u2013][a-zA-Z])?[a-zA-Z]*)*$"
+            pattern="^[A-Za-z.'\- ]+$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             className={css.form__input}
@@ -38,6 +38,7 @@ export const ContactForm = () => {
           <input
             type="tel"
             name="number"
+            pattern="^\+?\d{1,4}?\s?\(?\d{1,4}?\)?\s?\d{1,4}\s?\d{1,4}\s?\d{1,9}$"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             placeholder="Contact number"
